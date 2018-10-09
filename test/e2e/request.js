aea804ede4cdd399b8bfc76647c5ad800b773b58
Kevin Grayson <k.grayson.ms@gmail.com>
2018-10-09 16:49:11 -0700

    Flashcard POST test and route successful

--- test/e2e/request.js
+++ test/e2e/request.js
@@ -2,11 +2,18 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const http = require('http');
+const { assert } = chai;
 
 const app = require('../../lib/app');
 
 const server = http.createServer(app);
 const request = chai.request(server).keepOpen();
+
+request.checkOk = res => {
+    if(res.status !== 200) console.log('res status ', res.status);
+    assert.equal(res.status, 200, 'expected http 200 status code');
+    return res;
+};
 
 after(() => server.close());
 
