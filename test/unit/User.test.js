9b22c2b57f87e34ef9ef918b54ff549b13fc1b09
Easton Gorishek <ieaston26@gmail.com>
2018-10-09 15:01:18 -0700

    modified user model and corrected unit test

--- test/unit/User.test.js
+++ test/unit/User.test.js
@@ -4,7 +4,6 @@ const User = require('../../lib/models/User');
 
 describe('user model', () => {
     const credentials = {
-        name: 'Test',
         email: 'test@test.com',
         password: 'test123',
         roles: []
@@ -18,19 +17,19 @@ describe('user model', () => {
     });
 
     it('validates a good model', () => {
-        assert.equal(user.name, credentials.name);
         assert.equal(user.email, credentials.email);
         assert.isDefined(user.hash, 'hash is defined');
         assert.notEqual(user.hash, credentials.password, 'hash is different than password');
         assert.isTrue(user.comparePassword(credentials.password), 'compare good password');
         assert.isFalse(user.comparePassword('bad password'), 'compare bad password');
         assert.isUndefined(user.password, 'password has been hashed');
+        assert.isNotNull(user.roles, 'roles field exists');
     });
 
     it('validates required fields', () => {
         const user = new User({});
         const errors = getErrors(user.validateSync(), 3);
-        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.profileId.kind, 'required');
         assert.equal(errors.email.kind, 'required');
         assert.equal(errors.hash.kind, 'required');
     });
