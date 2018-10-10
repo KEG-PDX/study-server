63ba3282f325fef2ef2fc6524024a060fa8cf956
Kevin Grayson <k.grayson.ms@gmail.com>
2018-10-10 15:23:13 -0700

    Get a list of flashcards e2e test successful, save helper added

--- test/e2e/Flashcards.test.js
+++ test/e2e/Flashcards.test.js
@@ -1,8 +1,9 @@
 const { assert } = require('chai');
-const request = require('./request');
-const { dropCollection, createToken } = require('./db');
-const { checkOk } = request;
 const { Types } = require('mongoose');
+const request = require('./request');
+const { checkOk } = request;
+const { save } = require('../helpers');
+const { dropCollection, createToken } = require('./db');
 
 describe.only('Flashcards API', () => {
 
@@ -13,39 +14,39 @@ describe.only('Flashcards API', () => {
     beforeEach(() => createToken().then(t => token = t));
 
     let recursionFlashcard;
-    let codeProblem;
+    let getInfoFlashcard;
+    
     beforeEach(() => {
-        let flashcard = {
-            profileId: Types.ObjectId(),
-            category: 'Programming',
-            subCategory: 'Javascript',
-            question: 'What is a recursive function? Give an example of when using one might be useful.',
-            answer: 'Recursive functions call themselves until termination conditions are reached. A simple example would be a counter function that decrements a count until a given number (calling itself for each iteration).',
-        };
-        return request
-            .post('/api/flashcards')
-            .set('Authorization', token)
-            .send(flashcard)
-            .then(checkOk)
-            .then((res) => {
-                recursionFlashcard = res.body;
+        return save(
+            {
+                profileId: Types.ObjectId(),
+                category: 'Programming',
+                subCategory: 'Javascript',
+                question: 'What is a recursive function? Give an example of when using one might be useful.',
+                answer: 'Recursive functions call themselves until termination conditions are reached. A simple example would be a counter function that decrements a count until a given number (calling itself for each iteration).',
+            },
+            token,
+            'flashcards'
+        )
+            .then(data => {
+                recursionFlashcard = data;
             });
     });
+
     beforeEach(() => {
-        let flashcard = {
-            profileId: Types.ObjectId(),
-            category: 'Programming',
-            subCategory: 'General',
-            question: 'You can’t work out how to solve a coding problem. What do you do to find the answer?',
-            answer: 'First, look at the docs for the technology in question. Stack Overflow or other online resources could be considered second but if I get stuck after a set amount of time, it’s probably best to ask for help.',
-        };
-        return request
-            .post('/api/flashcards')
-            .set('Authorization', token)
-            .send(flashcard)
-            .then(checkOk)
-            .then((res) => {
-                recursionFlashcard = res.body;
+        return save(
+            {
+                profileId: Types.ObjectId(),
+                category: 'Programming',
+                subCategory: 'General',
+                question: 'You can’t work out how to solve a coding problem. What do you do to find the answer?',
+                answer: 'First, look at the docs for the technology in question. Stack Overflow or other online resources could be considered second but if I get stuck after a set amount of time, it’s probably best to ask for help.',
+            },
+            token,
+            'flashcards'
+        )
+            .then(data => {
+                getInfoFlashcard = data;
             });
     });
 
@@ -60,9 +61,9 @@ describe.only('Flashcards API', () => {
             .then(checkOk)
             .then(({ body }) => {
                 assert.deepEqual(body[0].category, recursionFlashcard.category);
-                assert.deepEqual(body[1].category, codeProblem.category);
+                assert.deepEqual(body[1].category, getInfoFlashcard.category);
                 assert.deepEqual(body[0].question, recursionFlashcard.question);
-                assert.deepEqual(body[1].question, codeProblem.question);
+                assert.deepEqual(body[1].question, getInfoFlashcard.question);
             });
     });
 });
